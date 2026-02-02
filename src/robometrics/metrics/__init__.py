5a56e9e50323b0c15014368ab85fce05e4fc919f
Nitish Sanghi <nitishsanghi@gmail.com>
2026-02-01 17:00:34 -0800

    metrics: add plugin loader for --plugins

--- src/robometrics/metrics/__init__.py
+++ src/robometrics/metrics/__init__.py
@@ -1,5 +1,6 @@
 """Metrics registry and helpers."""
 
 from robometrics.metrics.base import MetricContext, MetricSpec, REGISTRY, metric
+from robometrics.metrics.loader import load_plugins
 
-__all__ = ["MetricContext", "MetricSpec", "REGISTRY", "metric"]
+__all__ = ["MetricContext", "MetricSpec", "REGISTRY", "metric", "load_plugins"]
